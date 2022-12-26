feee4d183cc13da28aaf51d5f2279c0ab0b30f5e
xanqus <sbk98031816@gmail.com>
2022-12-26 18:32:51 +0900

    회원정보 등록 구현

---
+++ src/main/java/com/example/fastcampusmysql/domain/member/dto/RegisterMemberCommand.java
@@ -0,0 +1,12 @@
+package com.example.fastcampusmysql.domain.member.dto;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public record RegisterMemberCommand(
+        String email,
+        String nickname,
+        LocalDate birthday
+) {
+
+}
